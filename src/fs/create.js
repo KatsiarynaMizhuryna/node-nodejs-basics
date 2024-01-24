30605999d3181ddde13b3980dcbea516ad141241
Kate <ekaterina21051988@yandex.by>
2024-01-24 17:46:11 +0200

    feat: create.js function has been implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,24 @@
-const fs = require('fs').promises;
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require('node:fs/promises');
 const path = require('path');
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const create = async () => {
     try {
-        const filePath = path.join(__dirname, 'files', 'fresh.txt');
+        const filePath = path.join(__dirname,'files', 'fresh.txt');
 
         try {
-            await fs.access(filePath);
+            await fs.stat(filePath);
             throw new Error('FS operation failed: File already exists');
         } catch (err) {
             if (err.code === 'ENOENT') {
-             await fs.writeFile(filePath, 'I am fresh and young');
-             console.log('File created successfully: fresh.txt');
+                await fs.writeFile(filePath, 'I am fresh and young');
+                console.log('File created successfully: fresh.txt');
             } else {
-              throw err;
+                throw err;
             }
         }
     } catch (error) {
