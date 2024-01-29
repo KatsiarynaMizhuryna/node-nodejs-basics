8df18bc3d1c3fbf9d23ffb18842f301c18dfbe79
Kate <ekaterina21051988@yandex.by>
2024-01-29 15:34:35 +0200

    feat: parseEnv function has been created

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+const envPrefix = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  const filteredVariables = Object.entries(process.env)
+    .filter(([key]) => key.startsWith(envPrefix))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  if (filteredVariables) {
+    console.log(`Environment variables with prefix '${envPrefix}':`);
+    console.log(filteredVariables);
+  } else {
+    console.log(`No environment variables with prefix '${envPrefix}' found.`);
+  }
 };
 
 parseEnv();
