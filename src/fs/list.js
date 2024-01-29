bd0b065b5e95e78502e568e6279ffdb477e5f822
Kate <ekaterina21051988@yandex.by>
2024-01-29 13:52:43 +0200

    feat: list function has been created

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require("node:fs/promises");
+const path = require("path");
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const folderPath = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access(folderPath);
+    const files = await fs.readdir(folderPath);
+    console.log("Files in the folder:");
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (error) {
+    throw new Error("FS operation failed: Folder does not exist");
+  }
 };
 
 await list();
