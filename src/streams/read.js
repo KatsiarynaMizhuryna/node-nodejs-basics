4ea6120428ffee0c899868562ca7a78461e77d2d
Kate <ekaterina21051988@yandex.by>
2024-01-29 17:35:40 +0200

    feat: read function has been created

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from "node:fs";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const path = require("path");
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath, { encoding: "utf-8" });
+
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readStream.on("end", () => {
+    process.stdout.write("\n");
+  });
+
+  readStream.on("error", (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  });
 };
 
 await read();
