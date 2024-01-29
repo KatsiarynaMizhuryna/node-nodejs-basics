351cf9acc32676b6fcf113fa4b2e320d9ed9ee24
Kate <ekaterina21051988@yandex.by>
2024-01-29 13:57:28 +0200

    feat: read function has been created

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require("node:fs/promises");
+const path = require("path");
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToRead = "fileToRead.txt";
+const filePath = path.join(__dirname, "files", fileToRead);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await fs.access(filePath);
+    const content = await fs.readFile(filePath, "utf-8");
+    console.log(`Content of ${fileToRead}:`);
+    console.log(content);
+  } catch (error) {
+    throw new Error(`FS operation failed: File '${fileToRead}' does not exist`);
+  }
 };
 
 await read();
