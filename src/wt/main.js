0f9bc676ee7cbf1fcebc1141cd1612e487872376
Kate <ekaterina21051988@yandex.by>
2024-01-29 20:37:15 +0200

    feat: performCalculations function has been created

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from "node:worker_threads";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import os from "node:os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "worker.js");
+
+const numWorkers = os.cpus().length;
+const results = [];
+let numberToSend = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  for (let i = 0; i < numWorkers; i++) {
+    const workerPromise = new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, { workerData: numberToSend++ });
+      //worker.postMessage(numberToSend++);
+      worker.on("message", (message) => {
+        resolve(message);
+      });
+      worker.on("error", (error) => {
+        reject(error);
+      });
+    });
+    const workerResult = await workerPromise;
+    results.push({ status: "resolved", data: workerResult });
+  }
+
+  results.forEach((result) => console.log(`Result is ${result.data}`));
 };
 
 await performCalculations();
